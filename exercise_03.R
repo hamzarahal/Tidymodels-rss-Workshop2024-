1fec2df739a4e82ed5937c0a82dab2629b57f3a0
unknown <rahal.hmza@yahoo.fr>
2024-10-30 22:51:49 +0100

    Initial commit

---
+++ exercise_03.R
@@ -0,0 +1,36 @@
+set.seed(20240904)
+
+# Specify model -----------------------------------------------------------
+
+rf_tune_spec <- rand_forest(
+  mtry = tune(),
+  trees = 100,
+  min_n = tune()
+) |>
+  set_mode("classification") |>
+  set_engine("ranger")
+
+# Tune hyperparameters ----------------------------------------------------
+
+rf_grid <- tune_grid(
+  add_model(wf, rf_tune_spec),
+  resamples = resume_folds,
+  grid = grid_regular(mtry(range = c(5, 10)),
+                      min_n(range = c(2, 25)),
+                      levels = 3)
+)
+
+# Fit model ---------------------------------------------------------------
+
+rf_highest_roc_auc <- rf_grid |>
+  select_best(metric = "roc_auc")
+
+final_rf <- finalize_workflow(
+  add_model(wf, rf_tune_spec),
+  rf_highest_roc_auc
+)
+
+# Evaluate ----------------------------------------------------------------
+
+last_fit(final_rf, resume_split) |>
+  collect_metrics()
